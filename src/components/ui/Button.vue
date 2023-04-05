317b0c0fdf56d1d9adde6a65d7ff229a483fa580
Semen Ishinov <ectoplazm@gmail.com>
2023-04-05 11:44:07 +0300

    Add button component

---
+++ src/components/ui/Button.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+interface ButtonProps {
+  label?: string
+  disabled: boolean
+}
+
+const props = withDefaults(defineProps<ButtonProps>(), { disabled: false })
+const emit = defineEmits<{
+  (e: 'click'): void
+}>()
+</script>
+<template>
+  <button class="btn btn-sm btn-outline" :disabled="disabled" @click="$emit('click')">
+    <template v-if="label">{{ label }}</template>
+    <slot v-else></slot>
+  </button>
+</template>
