7f81b0595cb33bed6b7140f03868a1cd6e02a590
Semen Ishinov <ectoplazm@gmail.com>
2023-04-05 13:00:15 +0300

    add element plus and daisy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
+import Daisyui from 'daisyui'
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: [Daisyui]
 }
