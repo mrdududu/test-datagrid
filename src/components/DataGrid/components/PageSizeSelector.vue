e03a2510ef075849f3307dacbe0c0f5051368487
Semen Ishinov <ectoplazm@gmail.com>
2023-04-05 13:02:24 +0300

    add page size selector component

---
+++ src/components/DataGrid/components/PageSizeSelector.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ElSelect } from 'element-plus'
+import { ElOption } from 'element-plus'
+
+interface PageSizeSelectorProps {
+  itemsOnPage: number
+  pageSizeOptions: number[]
+}
+
+const props = defineProps<PageSizeSelectorProps>()
+const emit = defineEmits<{
+  (e: 'change', itemsOnPage: number): void
+}>()
+</script>
+<template lang="pug">
+.flex
+  div Show 
+  div
+    el-select(:modelValue="itemsOnPage" @update:modelValue="$emit('change', $event)")
+      el-option(v-for="pSize in pageSizeOptions" :key="pSize" :value="pSize" :label="`${pSize}`")
+  div entries
+</template>
