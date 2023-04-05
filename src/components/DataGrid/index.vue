423e1768b1cc17dedbd0e7f71a942046e29196f6
Semen Ishinov <ectoplazm@gmail.com>
2023-04-05 13:02:45 +0300

    Bind components to the store

--- src/components/DataGrid/index.vue
+++ src/components/DataGrid/index.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { useDataGridStore } from '@/stores/data-grid'
 import Grid from './components/Grid.vue'
+import Paging from './components/Paging.vue'
+import PageSizeSelector from './components/PageSizeSelector.vue'
 import type { Column } from './'
 
 const columns: Column[] = [
@@ -9,6 +11,8 @@ const columns: Column[] = [
   { name: 'title', label: 'title', sortable: true }
 ]
 
+const pageSizeOptions = [3, 10, 20, 23]
+
 const store = useDataGridStore()
 store.actions.fetchData()
 
@@ -16,7 +20,21 @@ const handleSortToggle = (nameColumn: string) => {
   console.log('handleSortToggle', nameColumn)
   store.actions.toogleSorting(nameColumn)
 }
+
+const handleSetPage = (pageIndex: number) => {
+  console.log('handleSetPage', pageIndex)
+  store.actions.setCurrentPageIndex(pageIndex)
+}
+
+const handelItemsOnPageChange = (itemsOnPage: number) => {
+  console.log('handelItemsOnPageChange', itemsOnPage)
+  store.actions.setItemsOnPage(itemsOnPage)
+}
 </script>
 <template lang="pug">
-Grid(:columns="columns" :rows="store.getters.currentPageData" @sortToggle="handleSortToggle")
+div
+  div
+    PageSizeSelector(:itemsOnPage="store.state.itemsOnPage" :pageSizeOptions="pageSizeOptions" @change="handelItemsOnPageChange")
+  Grid(:columns="columns" :rows="store.getters.currentPageData" @sortToggle="handleSortToggle")
+  Paging(:pageIndex="store.state.currentPageIndex" :totalPages="store.getters.totalPages" @setPage="handleSetPage")
 </template>
